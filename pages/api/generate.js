826d3bb6154e941b25e036c1e4b99515d5baa91f
Yuki-J-Klang <Yuki.i.000915@gmail.com>
2023-01-14 02:06:11 +0900

    fix: api,index

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -1,31 +1,27 @@
 import { Configuration, OpenAIApi } from "openai";
-  
-const UserUsedWords = [];
-const AiUsedWords = [];
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
-
 export default async function (req, res) {
-
   if (!configuration.apiKey) {
     res.status(500).json({
       error: {
         message: "OpenAI API key not configured",
-      }
+      },
     });
     return;
   }
 
-  const words = req.body.words || '';
-  if (words.trim().length === 0) {
+  const word = req.body.word || "";
+
+  if (word.trim().length === 0) {
     res.status(400).json({
       error: {
         message: "文字を入れてください。",
-      }
+      },
     });
     return;
   }
@@ -33,34 +29,16 @@ export default async function (req, res) {
   try {
     const completion = await openai.createCompletion({
       model: "text-davinci-003",
-      prompt: generatePrompt(words) || "次は、貴方のターンです。",
+      prompt: generatePrompt(word),
       temperature: 0,
     });
-    
-    UserUsedWords.push = words;
-    result = completion.data.choices[0].text;
 
-    if(words === 'ん' || words === 'ン'){
-      result = "「ん」または「ン」がついているので、貴方の負けです。";
-      res.status(200).json(result);
-    } else if(UserUsedWords.includes(words) || AiUsedWords.includes(words)){
-      result = "既出の単語を使用したので、貴方の負けです。"
-      res.status(200).json(result);
-    } else if(result.slice(-1) == 'ん' || result.slice(-1) == 'ン') {
-      completion;
-      res.status(200).json({ result: completion.data.choices[0].text });
-      AiUsedWords.push = result;
-    } else if(UserUsedWords.includes(result) || AiUsedWords.includes(result)){
-      completion;
-      res.status(200).json({ result: completion.data.choices[0].text });
-      AiUsedWords.push = result;
-    } else {
-      res.status(200).json({ result: completion.data.choices[0].text });
-      AiUsedWords.push = result;
-    }
-    
-
-  } catch(error) {
+    console.log(word);
+    console.log(generatePrompt(word));
+    res.json({
+      result: completion.data.choices[0].text.trim().replaceAll("\n", ""),
+    });
+  } catch (error) {
     if (error.response) {
       console.error(error.response.status, error.response.data);
       res.status(error.response.status).json(error.response.data);
@@ -68,22 +46,21 @@ export default async function (req, res) {
       console.error(`Error with OpenAI API request: ${error.message}`);
       res.status(500).json({
         error: {
-          message: 'An error occurred during your request.',
-        }
+          message: "An error occurred during your request.",
+        },
       });
     }
   }
 }
 
-function generatePrompt(words) {
-  const replyWords = words.slice(-1);
-  return `Userから送られた単語の最後の音節から始まり、末尾が「ん」にならない単語を1つあげてください。同じ単語は使ってはいけません。
-  User: アザラシ
-  Ai: 屍
-  User: 粘土
-  Ai: ドミニカ共和国
-  User: クリスマス
-  Ai: するめ
-  User: ${replyWords}
-  Ai:`;
+function generatePrompt(word) {
+  const replyWord = word;
+  const lastWord = replyWord.slice(-1);
+
+  return `
+  しりとり中です。
+  「${lastWord}」から始まる単語を返してください。
+  最後の文字が「ん」または「ン」になってはいけません。
+  暴言や悪口、汚い言葉は禁止です。
+  `;
 }
--- pages/index.jsx
+++ pages/index.jsx
@@ -3,34 +3,51 @@ import styles from "./index.module.css";
 import { useState } from "react";
 
 export default function Home() {
-  const [wordsInput, setWordsInput] = useState("");
-  const [result, setResult] = useState();
+  const [wordInput, setWordInput] = useState("");
+  const [usedWords, setUsedWords] = useState([]);
+  const [resultMsg, setResultMsg] = useState("");
 
   async function onSubmit(event) {
     event.preventDefault();
+
+    setUsedWords((prev) => [...prev, wordInput]);
+
     try {
       const response = await fetch("/api/generate", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ words: wordsInput }),
+        body: JSON.stringify({ word: wordInput }),
       });
 
       const data = await response.json();
       if (response.status !== 200) {
-        throw data.error || new Error(`Request failed with status ${response.status}`);
+        throw (
+          data.error ||
+          new Error(`Request failed with status ${response.status}`)
+        );
       }
 
-      setResult(data.result);
-      setWordsInput("");
-    } catch(error) {
-      
+      if (["ん", "ン"].includes(wordInput.slice(-1))) {
+        setResultMsg("「ん」または「ン」がついているので、貴方の負けです。");
+      } else if (usedWords.includes(wordInput) === true) {
+        setResultMsg("既出の単語を使用したので、貴方の負けです。");
+      } else if (["ん", "ン"].includes(data.result.slice(-1))) {
+        setResultMsg("「ん」または「ン」がついているので、貴方の勝ちです。");
+      } else if (usedWords.includes(data.result) === true) {
+        setResultMsg("既出の単語を使用したので、貴方の勝ちです。");
+      }
+
+      setUsedWords((prev) => [...prev, data.result]);
+      setWordInput("");
+    } catch (error) {
       console.error(error);
       alert(error.message);
     }
   }
 
+  // console.log(usedWords);
   return (
     <div className={styles.body}>
       <Head>
@@ -40,20 +57,20 @@ export default function Home() {
         <h3>SAHIRITORⅡ</h3>
       </header>
       <main className={styles.main}>
-        <form onSubmit={onSubmit}>
+        {usedWords.length > 0 && !resultMsg && (
+          <p className={styles.font}>{usedWords.join(", ")}</p>
+        )}
+        {resultMsg && <p className={styles.font}>{resultMsg}</p>}
+        <form className={styles.form} onSubmit={onSubmit}>
           <input
             type="text"
-            name="words"
-            placeholder="Enter an words"
-            value={wordsInput}
-            onChange={(e) => setWordsInput(e.target.value)}
+            name="word"
+            placeholder="Enter a word"
+            value={wordInput}
+            onChange={(e) => setWordInput(e.target.value)}
           />
-          <input 
-            type="submit" 
-            value="Send words"
-          />
+          <button type="submit">Send word</button>
         </form>
-        <div className={styles.result}>{result}</div>
       </main>
     </div>
   );
